78fd3b207366b9f98770299de08ab0dc0ccbdcea
Geo <megagog19@gmail.com>
2020-03-26 18:12:13 +0200

    Good working laba

---
+++ src/main/java/com/example/feign/controllers/ConfigController.java
@@ -0,0 +1,24 @@
+package com.example.feign.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RefreshScope
+public class ConfigController {
+
+    @Value("${proj.description: None}")
+    private String projDesc;
+
+    @Value("${app.description: None}")
+    private String appDesc;
+
+    @RequestMapping("/desc")
+    @ResponseBody
+    public String getPropsFormDownloadedConfig() {
+        return "Project: " + projDesc + " | " + "App: " + appDesc;
+    }
+}
